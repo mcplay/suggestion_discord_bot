82a93e9049e41fbbd5b4dcfdd4264f8dde9667d1
snake <admin@viasnake.com>
2020-02-15 00:47:34 +0900

    Add reaction in faq and update-log

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     if message.channel.name == "suggestion":
-        await message.add_reaction("✅")
-        await message.add_reaction("❌")
+        await message.add_reaction(":white_check_mark:")
+        await message.add_reaction(":x:")
+    if message.channel.name == "faq":
+        await message.add_reaction(":thumbsup:")
+        await message.add_reaction(":thumbsdown:")
+    if message.channel.name == "update-log":
+        await message.add_reaction(":thumbsup:")
+        await message.add_reaction(":thumbsdown:")
 
 client.run(os.environ["TOKEN"])
